22ea83a22d6c58d9b250f777583b7f2a65ffb93f
Quince Lewis <106302729+quinceosanya@users.noreply.github.com>
2022-06-22 09:25:27 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// Code your solution in this file!
+
+describe('index.js', function () {
+    // there's stuff in here, too
+  });
+  it('is set as Scuber', function () 
+    // tests are here
+  });
+ it('is set as Scuber', function () {
+   expect(companyName).to.equal('Scub
+ });
+ let companyName = 'Scuber';
+
